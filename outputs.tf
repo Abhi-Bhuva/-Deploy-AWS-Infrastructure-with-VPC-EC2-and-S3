858784fe7c568b8dfedc449170cdb5596760f6c8
Abhi-Bhuva <158251667+Abhi-Bhuva@users.noreply.github.com>
2025-07-04 17:06:42 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "ec2_:public_ip" {
-  description = "Public IP of EC2 Instance"
-  value       = aws_instance.web.public_ip
-  }
-
-output "bucket_name" {
-  value = aws_s3_bucket.bucket.bucket
-}
