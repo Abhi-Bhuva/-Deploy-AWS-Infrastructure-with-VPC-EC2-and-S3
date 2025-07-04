32c9e7109ead044d8d3cd75e5f4a4ed3456e47c7
Abhi-Bhuva <158251667+Abhi-Bhuva@users.noreply.github.com>
2025-07-04 15:57:20 +0530

    Create varaibles.tf

---
+++ varaibles.tf
@@ -0,0 +1,9 @@
+variable "key_name" {
+  description = "Existing EC2 Key Pair Name"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Unique S3 bucket name"
+  type        = string
+}
