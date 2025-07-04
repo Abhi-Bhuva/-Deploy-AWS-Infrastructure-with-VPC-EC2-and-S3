7015ec2affa96dc948a52fc450238733575b4b14
Abhi-Bhuva <158251667+Abhi-Bhuva@users.noreply.github.com>
2025-07-04 17:05:57 +0530

    Delete varaibles.tf

--- varaibles.tf
+++
@@ -1,9 +0,0 @@
-variable "key_name" {
-  description = "Existing EC2 Key Pair Name"
-  type        = string
-}
-
-variable "bucket_name" {
-  description = "Unique S3 bucket name"
-  type        = string
-}
